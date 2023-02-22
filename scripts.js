2875e92db4270c735b374ceb10651cf0a70c31ef
slaveharold <116307057+slaveharold@users.noreply.github.com>
2023-02-22 00:42:39 -0600

    reset

--- scripts.js
+++ scripts.js
@@ -21,23 +21,3 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-const sendMessageButton = document.getElementById("pat");
-sendMessageButton.addEventListener("click", () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://discord.com/api/webhooks/1077831699318177912/bkk2FM-T2hvjsdNeyd4y-SwDx3d0i6q0gjHkHEvs1BKeFyqWbyZWfrrQgLlzMlenP0Pp");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({
-    content: "Pat Pat"
-  }));
-});
-
-const sendMessageButton = document.getElementById("checks");
-sendMessageButton.addEventListener("click", () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://discord.com/api/webhooks/1077837990795165716/YjFsGSEAbqUd7IDc3i5vFAYwJuPj5fdi6K8aVeXLtbzoOMIUd84QczDaUrHDQtyUjA83");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({
-    content: "Schedule"
-  }));
-});
